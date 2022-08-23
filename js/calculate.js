2cab3feba593fd683d6b3c18e299d756f4de5e0d
farukphero <farukk6319@gmail.com>
2022-08-23 14:06:17 +0600

    Add disable button

--- js/calculate.js
+++ js/calculate.js
@@ -2,11 +2,11 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
   const perPlayerInputAmount = document.getElementById('input-amount')
   const perPlayerInputAmountValueString = perPlayerInputAmount.value;
   const perPlayerInputAmountValue = parseInt(perPlayerInputAmountValueString)
-  
+  perPlayerInputAmount.value=''
   const totalAddedPlayer = document.getElementById('total-added-player');
   const totalAddedPlayerValueString = totalAddedPlayer.value;
   const totalAddedPlayerValue = parseInt(totalAddedPlayerValueString)
-   
+   totalAddedPlayer.value=''
 
   const playerExpenses = totalAddedPlayerValue * perPlayerInputAmountValue;
   
@@ -26,11 +26,11 @@ document.getElementById('total-calculate-btn').addEventListener('click', functio
   const managerExpenses = document.getElementById('manager-expenses');
   const managerExpensesValueString = managerExpenses.value;
   const managerExpensesValue = parseInt(managerExpensesValueString);
-
+  managerExpenses.value = '';
   const coachExpenses = document.getElementById('coach-expenses');
   const coachExpensesValueString = coachExpenses.value;
   const coachExpensesValue = parseInt(coachExpensesValueString);
-
+  coachExpenses.value = '';
   const toatalExpenses = totalPlayerExpensesValue + managerExpensesValue + coachExpensesValue;
   console.log(managerExpensesValue, coachExpensesValue, toatalExpenses)
 
--- js/name.js
+++ js/name.js
@@ -11,10 +11,44 @@ function displayPlayerName(updatePlayerName) {
     `;
     createList.appendChild(li)
   }
+  
 }
 function addToPlayer(element) {
   const PlayerName = element.parentNode.children[1].innerText;
   playerArray.push(PlayerName)
   document.getElementById('total-added-player').value = playerArray.length;
   displayPlayerName(playerArray)
+};
+
+// function of disable button after click :
+
+function disableBtn(inputBtn) {
+  document.getElementById(inputBtn).disabled = true;
+  document.getElementById(inputBtn).innerText = 'Already Selected';
+  document.getElementById(inputBtn).style.backgroundColor = 'gray';
 }
+document.getElementById('first-btn').addEventListener('click', function () {
+  disableBtn('first-btn')
+  
+});
+document.getElementById('second-btn').addEventListener('click', function () {
+  disableBtn('second-btn');
+   
+});
+document.getElementById('third-btn').addEventListener('click', function () {
+  disableBtn('third-btn')
+   
+});
+document.getElementById('fourth-btn').addEventListener('click', function () {
+  disableBtn('fourth-btn')
+   
+});
+document.getElementById('fifth-btn').addEventListener('click', function () {
+  disableBtn('fifth-btn')
+    
+});
+document.getElementById('six-btn').addEventListener('click', function () {
+  disableBtn('six-btn')
+  
+   
+});
