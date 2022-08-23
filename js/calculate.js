fe1b29dacb1c908ffadfbfb479e280b442c0e7c3
farukphero <farukk6319@gmail.com>
2022-08-23 12:31:47 +0600

    Calculate Final Expenses

--- js/calculate.js
+++ js/calculate.js
@@ -1,20 +1,44 @@
 document.getElementById('calculate-btn').addEventListener('click', function () {
   const perPlayerInputAmount = document.getElementById('input-amount')
   const perPlayerInputAmountValueString = perPlayerInputAmount.value;
-  const perPlayerInputAmountValue =parseInt(perPlayerInputAmountValueString)
-  // console.log(perPlayerInputAmountValue)
+  const perPlayerInputAmountValue = parseInt(perPlayerInputAmountValueString)
+  
   const totalAddedPlayer = document.getElementById('total-added-player');
   const totalAddedPlayerValueString = totalAddedPlayer.value;
   const totalAddedPlayerValue = parseInt(totalAddedPlayerValueString)
-  // console.log(totalAddedPlayerValue);
+   
 
   const playerExpenses = totalAddedPlayerValue * perPlayerInputAmountValue;
-  console.log(playerExpenses)
+  
 
-  const totalExpenses = document.getElementById('total-expenses');
-  const totalExpensesValueString = totalExpenses.innerText;
-  const totalExpensesValue = parseInt(totalExpensesValueString);
-  totalExpenses.innerText = playerExpenses;
-  console.log(totalExpensesValue)
+  const totalPlayerExpenses = document.getElementById('total-expenses');
+  const totalPlayerExpensesValueString = totalPlayerExpenses.innerText;
+  const totalPlayerExpensesValue = parseInt(totalPlayerExpensesValueString);
+  totalPlayerExpenses.innerText = playerExpenses;
+   
+});
 
- })
+document.getElementById('total-calculate-btn').addEventListener('click', function () {
+  const totalPlayerExpenses = document.getElementById('total-expenses');
+  const totalPlayerExpensesValueString = totalPlayerExpenses.innerText;
+  const totalPlayerExpensesValue = parseInt(totalPlayerExpensesValueString);
+  
+  const managerExpenses = document.getElementById('manager-expenses');
+  const managerExpensesValueString = managerExpenses.value;
+  const managerExpensesValue = parseInt(managerExpensesValueString);
+
+  const coachExpenses = document.getElementById('coach-expenses');
+  const coachExpensesValueString = coachExpenses.value;
+  const coachExpensesValue = parseInt(coachExpensesValueString);
+
+  const toatalExpenses = totalPlayerExpensesValue + managerExpensesValue + coachExpensesValue;
+  console.log(managerExpensesValue, coachExpensesValue, toatalExpenses)
+
+
+  const finalExpenses = document.getElementById('final-expenses');
+  const finalExpensesValueString = finalExpenses.innerText;
+  const finalExpensesValue = parseInt(finalExpensesValueString)
+  finalExpenses.innerText = toatalExpenses;
+  
+
+})
